68233997ecbfe70118eb95903933a2063884fb65
Zachary Vanscoit <zachary816@gmail.com>
2019-08-23 15:25:34 -0500

    Cleaned up some code for use from console

--- Java Projects/VisualLinearDisplays/src/core/LinearApplet.java
+++ Java Projects/VisualLinearDisplays/src/core/LinearApplet.java
@@ -7,55 +7,103 @@ public class LinearApplet extends PApplet {
 	
 	// main and initialization of applet
 	public static void main(String args[]) {
+		// Handle command line arguments
+		try {
+			if (args[0] != null) {
+				pointScatter = Integer.parseInt(args[0]);
+			}
+			if (args[1] != null) {
+				cyclesPerSecond = Integer.parseInt(args[1]);
+			}
+			if (args[2] != null) {
+				slope = (float) Double.parseDouble(args[2]);
+			}
+			if (args[3] != null) {
+				numberOfPoints = Integer.parseInt(args[3]);
+			}
+			// Catch all problems and literally ignore them
+		} catch (Exception e) {
+			System.out.println("This is the error: " + e.getMessage());
+			System.out.println("Format is:\n[point scatter] [frame rate] [slope] [number of points]");
+		}
+		
+		System.out.println("\nRunning app with these variables...\n"
+				+ "pointScatter: " + pointScatter + "\n"
+				+ "frameRate: " + cyclesPerSecond + "\n"
+				+ "slope: " + slope + "\n"
+				+ "numberOfPoints: " + numberOfPoints + "\n");
+		
 		PApplet.main("core.LinearApplet");
 	}
 	// Settings of applet
 	public void settings() {
-		size(800, 800);
+		size(1200, 1200);
 	}
 	
+	// Processing Variables
+	private static int cyclesPerSecond = 60;				// Default fps 60
+	
 	// Graph variables
-	private PVector[] points = new PVector[100];	// Default 100 points
-	private float slope = 1.1f;						// Default 1 for slope
-	private int scale = 8;							// Default 10 for size
-	private int pointScatter = 120;					// Default 50 for deviance
+	private static int numberOfPoints = 100;				// Default 100 points
+	private static float slope = 1.1f;						// Default 1 for slope
+	private static int scale = 8;							// Default 10 for size
+	private static int pointScatter = 120;					// Default 50 for deviance
+	private static boolean drawGrid = false;
+	private PVector[] points;
+	
+	// Guess Variables
+	// Try not to mess with these as they change while its running.
+	private float guessSlope = 4;
+	private float minimumDistance = 9999999;
+	private float minimumSlope = 1;
+	private boolean toggle = true;
 	
 	// Setup and initial variable assigning.
 	public void setup() {
-		surface.setTitle("Linear R Assumption");
-		frameRate(60);
+		surface.setTitle("Visual Linear R");
+		frameRate(cyclesPerSecond);
 		
-		// Initializers
+		scale = width / numberOfPoints;
+		int pointsOffScreen = 0;
+		
+		// Initialize points
+		points = new PVector[numberOfPoints];
 		int x = 0, y = 0;
 		for (int i = 0; i < points.length; i ++) {
 			points[i] = new PVector(x + (int) random(-pointScatter, pointScatter),
 									y + (int) random(-pointScatter, pointScatter));
+			if ((points[i].x > width) | (points[i].y > height)) {
+				pointsOffScreen ++;
+			}
 			x += scale;
 			y = (int) (x * slope);
 		}
+		println("points off screen: " + pointsOffScreen);
+		println("scale: " + scale + "\nFinished Setup...");
 	}
-	
-
-	// Guess Variables
-	private float guessSlope = 4;
-	private float minimumDistance = 9999999;
-	private float minimumSlope = 1;
 	
 	public void draw() {
 		background(40);
 		noFill();
 		
+		// We push the matrix to perform translations without affecting the camera
 		pushMatrix();
 		rotate(PI / -2);
 		translate(-width, 0);
-		// Draw Graph
+		
+		// Draw Grid Lines
 		stroke(100);
-		strokeWeight(1);
-		for (int x = 0; x < width; x += scale) {
-			for (int y = 0; y < height; y += scale) {
-				rect(x, y, scale, scale);
+		if (drawGrid) {
+			strokeWeight(0.75f);
+			for (int x = 0; x < width; x += scale) {
+				for (int y = 0; y < height; y += scale) {
+					rect(x, y, scale, scale);
+				}
 			}
 		}
+		strokeWeight(4);
+		line(0, 0, width, 0);
+		line(1, 0, 1, height);
 
 		// Draw points
 		stroke(255);
@@ -75,7 +123,7 @@ public class LinearApplet extends PApplet {
 	
 	private void drawGuessData() {
 		float sumOfDistance = 0;
-		if (guessSlope > 0) {
+		if (guessSlope >= 0.001) {
 			stroke(0, 200, 255);
 			line(0, 0, width, height * guessSlope);
 			strokeWeight(0.5f);
@@ -97,6 +145,13 @@ public class LinearApplet extends PApplet {
 				minimumSlope = guessSlope;
 			}
 			guessSlope -= 0.01f;
+		} else {
+			guessSlope = 0;
+			if (toggle) {
+				println("Finished drawing...");
+				println("\nMinimum slope: " + minimumSlope);
+				toggle = false;
+			}
 		}
 	}
 	
@@ -113,11 +168,16 @@ public class LinearApplet extends PApplet {
 	// Draws the text on screen
 	private void drawUI() {
 		noStroke();
-		fill(40);
-		rect(width - 110, height - 36, 110, 36);
+		textSize(20);
+		textAlign(RIGHT);
 		fill(255);
-		textSize(14);
-		text("Slope: " + guessSlope, width - 100, height - 18);
-		text("Min: " + minimumSlope, width - 100, height - 2);
+		
+		if (guessSlope >= 0.001) {	
+			text("Slope: " + guessSlope, width - 20, height - 45);
+		} else {
+			text("Initial slope: " + slope, width - 20, height - 45);
+			fill(0, 200, 255);
+		}
+		text("Min: " + minimumSlope, width - 20, height - 20);
 	}
 }
